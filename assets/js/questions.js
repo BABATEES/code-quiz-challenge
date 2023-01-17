fb478f189d506ad47f7a6500809f1b98519c1234
oluwatayo Hassan <tayohas@gmail.com>
2023-01-17 21:59:26 +0000

    added quiz questions to the js file

---
+++ assets/js/questions.js
@@ -0,0 +1,57 @@
+var Quizquestions= [
+    {
+        Question: "What Does HTML Stand For?",
+        options:[
+        "Hypertext Markup Language",
+        "Hypertext Markdown Language",
+        "Hyperloop Machine Language",
+        "Hypotext Markup language",
+        ],
+        Answer: "A"
+    },
+    {
+    Question: "What Does CSS Stand For?",
+    options:[
+    "Central Style Sheets",
+    "Cascading Style Sheets",
+    "Cascading Simple Sheets",
+    "Cascading style sheet",
+    ],
+    Answer: "B",
+    },
+
+    {Question: "Which Language Runs In A Web Browser?",
+    options:
+    [
+    "Java",
+    "C",
+    "Python",
+    "JavaScript",
+    ],
+    Answer: "D",
+},
+    {
+    Question: "What Year Was JavaScript Launched?",
+    options:
+    [
+     "1996",
+     "1995",
+    "1994",
+    "None Of The Above",
+    ],
+    
+    Answer: "B",
+
+    },
+    {
+        Question: "what is full meaning of DOM in Java?",
+       options: [
+        "Document object Model",
+        "Document object Modem",
+        "Document object Modern",
+        "None Of The Above",
+    ],
+        Answer: "A",
+    
+        },
+    ];
