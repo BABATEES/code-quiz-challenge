afa1d3480320f6d473cf9c5d8cf9a7c0e65eb4e0
oluwatayo Hassan <tayohas@gmail.com>
2023-01-21 19:15:27 +0000

    created function to display question on the logic file

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,7 +1,7 @@
 console.log("Answer the following:" + questions);
 
-var questionsdiv= document.querySelector("#questions");
-var questionstitle= document.querySelector("#question-title");
+var questionsdiv = document.querySelector("#questions");
+var questionstitle = document.querySelector("#question-title");
 var startScreen = document.querySelector("#start-screen");
 var startButton = document.querySelector("#start");
 var choices = document.querySelector("#choices");
@@ -10,5 +10,30 @@ var endscreen = document.querySelector("#end-screen")
 var feedback = document.querySelector("#feedback")
 var currentQuestionIndex = 0;
 
-var timer =0
+var timer = 0
+
+//Created function to display the questions for answering
+function showQuestions() {
+
+  startScreen.setAttribute("class", "hide");
+  questionsdiv.setAttribute("class", "start");
+  timeSpan.textContent = timer;
+  getquestions();
+
+}
+
+function getquestions() {
+  var currentQuestion = questions[currentQuestionIndex];
+  console.log(currentQuestion);
+  questionstitle.textContent = currentQuestion.question;
+  for (i = 0; i < currentQuestion.options.length; i++) {
+    var choice = document.createElement("button");
+    choice.textContent = currentQuestion.options[i];
+    choice.onclick = handleclick;
+    choices.appendChild(choice);
+  }
+
+
+
+}
 
