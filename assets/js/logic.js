7e90e91d3cfeadfaba3380a6bfcdf4c45825e87c
oluwatayo Hassan <tayohas@gmail.com>
2023-01-21 19:12:30 +0000

    updated the logic file with var declared for each div id

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,2 +1,14 @@
+console.log("Answer the following:" + questions);
 
+var questionsdiv= document.querySelector("#questions");
+var questionstitle= document.querySelector("#question-title");
+var startScreen = document.querySelector("#start-screen");
+var startButton = document.querySelector("#start");
+var choices = document.querySelector("#choices");
+var timeSpan = document.querySelector("#time");
+var endscreen = document.querySelector("#end-screen")
+var feedback = document.querySelector("#feedback")
+var currentQuestionIndex = 0;
+
+var timer =0
 
